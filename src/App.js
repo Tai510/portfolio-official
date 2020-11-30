7ef59e4b0f732e64fa000958f330366c908b5571
Tashi Woeser <tashiwoeser@Tashis-MacBook-Pro.local>
2020-11-29 18:09:24 -0800

    deploy failed trying again

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import "./App.css";
 import Footer from "./components/Footer/footer";
 import HomePage from "./components/Home-Page/home";
-import AboutPage from "./components/About-Page/about";
-import Projects from "./components/Projects/projects";
-import Contact from "./components/Contact/contact";
-import Resume from './components/Resume/Resume';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
